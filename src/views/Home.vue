e2cdaea3a5826b53d1737751d53a984c2f1a2e1d
Daniel Lucchesi <dlucchesi@gmail.com>
2023-03-13 15:53:06 -0300

    Deleted cache test

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,7 +8,7 @@
       <div class="w-full bg-gray-800 rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
         <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
           <h1 class="text-xl font-bold leading-tight tracking-tight text-light-900 md:text-2xl dark:text-white">
-            Welcome to MyGlic 1
+            Welcome to MyGlic
           </h1>
         </div>
       </div>
